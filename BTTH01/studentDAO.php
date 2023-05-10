129df5a76b6faef3cc0ef066c259008ca34db06d
Levan71 <2051063471@e.tlu.edu.vn>
2023-05-10 23:10:36 +0700

    update form and class student

---
+++ BTTH01/studentDAO.php
@@ -0,0 +1,15 @@
+<?php
+class studentDAO{
+    private $student = array();
+
+    public function create($student)
+    {
+        array_push($this->student=$student);
+    }
+    public function read($id)
+    {
+        
+    }
+}
+
+?>
